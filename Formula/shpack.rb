79bb5b8dc3a3c85d7c82b0d588e1bf8c579d0410
nguyen <luongnguyen1805@gmail.com>
2025-10-10 09:22:37 +0700

    Update Page url of release.

--- Formula/shpack.rb
+++ Formula/shpack.rb
@@ -1,7 +1,7 @@
 class Shpack < Formula
   desc "Shell Script Bundler - package multiple scripts into a single executable"
   homepage "https://github.com/luongnguyen1805/shpack"
-  url "https://github.com/luongnguyen1805/shpack/releases/download/1.0.0/shpack-1.0.0.tar.gz"
+  url "https://luongnguyen1805.github.io/shpack/shpack-1.0.0.tar.gz"
   sha256 "d8b69ee6cf9434a8fe7005b1be2c98aada99b5090e233bb20548b0b3173406ea"
   license "MIT"
   version "1.0.0"
